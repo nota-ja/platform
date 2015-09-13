3f8c79661ebf1f1aafb5cfcfbb9c00650b58f276
Noburou TANIGUCHI <dev@nota.m001.jp>
2015-09-14 08:25:33 +0900

    Add initial version of startup script

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x
+
+mkdir -p web/static/js
+
+echo starting react processor
+pushd web/react
+npm start &
+popd
+
+echo starting compass watch
+pushd web/sass-files
+compass watch &
+popd
+
+echo starting go web server
+platform -config config/config.json || nc -l -k $PORT
